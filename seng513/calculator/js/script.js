561ec7530bd84e488ba0368be955c2272146a2a4
Daniel <chiefgustavo000@gmail.com>
2020-02-06 15:38:49 -0700

    Created the process_calc and added event listener for operator so far

--- seng513/calculator/js/script.js
+++ seng513/calculator/js/script.js
@@ -1,3 +1,30 @@
-window.addEventListener("load", function(){
-    console.log("Hello World");
-});
+// Wait until html and css is fully loaded before executing javascript
+window.addEventListener("load", process_calculator);
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+// using event listener with arrow function
+function process_calculator(){
+    console.log("function process_calculator");
+    const calc_keys = document.querySelector(".calculator_buttons");
+    console.log(calc_keys);
+    calc_keys.addEventListener('click', (event) => {
+        const {target} = event;
+        if(!target.matches("button")){
+            console.log("Button was not pressed");
+            return;
+        }
+        if(target.className === "operator"){
+            console.log("Operator", target.value);
+            return;
+        }
+    });
+}
+
+function get_key_pressed(){
+    console.log("Getting key pressed");
+}
+function update_display(){
+    const output = document.querySelector('input_area');
+    output.value = calculator.display_value;
+}
