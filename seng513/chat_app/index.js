3d01db73008ac4ad86af0844bf39bf9522d58ff6
Daniel <chiefgustavo000@gmail.com>
2020-02-23 15:54:39 -0700

    Added the socket connection

--- seng513/chat_app/index.js
+++ seng513/chat_app/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
-
 const http = require('http').createServer(app);
 
+const socket_io = require('socket.io');
+var io = socket_io(http);
+
+
 const PORT = 3000;
+// this is for static files like my css file, to enable it to work properly
+app.use(express.static(__dirname + '/'));
 // route handler that's called when we hit our website home
 app.get('/', (req, res) => {
-    res.send(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
+});
+
+//listen on the connection event for incoming sockets
+io.on('connection', (socket) => {
+    console.log('a user connected')
 });
 
 // our http server listens to port 3000
