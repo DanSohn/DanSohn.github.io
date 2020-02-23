eebecafbf4444d10c5aa764ef86f1402d43cc430
Daniel <chiefgustavo000@gmail.com>
2020-02-23 16:06:04 -0700

    I show the message that's sent to my terminal

--- seng513/chat_app/index.js
+++ seng513/chat_app/index.js
@@ -15,10 +15,16 @@ app.get('/', (req, res) => {
 });
 
 //listen on the connection event for incoming sockets
+// if i want to send to everyone except for myself, use socket.broadcast.emit('hi')
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
