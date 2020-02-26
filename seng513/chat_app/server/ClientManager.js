da63ea02659cdf1f38a1100d77ed5816bba2954b
Daniel <chiefgustavo000@gmail.com>
2020-02-25 22:36:59 -0700

    Adding additional event handlers

--- seng513/chat_app/server/ClientManager.js
+++ seng513/chat_app/server/ClientManager.js
@@ -1,4 +1,4 @@
-function handleRegister(user_name, callback){
+function handle_register(user_name, callback){
     // checks if the username is in use: if not, return a callback with error statement : in use. Doesn't return a result
     if(!clientManager.isUserAvailable(user_name)){
         return callback("Username is already in use. Choose another username");
@@ -9,4 +9,13 @@ function handleRegister(user_name, callback){
 
     // callback (error, result) is the template
     return callback(null, user);
+}
+
+
+function handle_event(chatroom, create_entry){
+    return check_chatroom(chatroom)
+        //returns a promise
+        .then(function({room, user}) {
+            let entry = 
+        })
 }
