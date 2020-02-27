dde17a07570926098a7456760398ddb66a25e119
Daniel Sohn <chiefgustavo000@gmail.com>
2020-02-26 18:45:56 -0700

    Original file

--- seng513/chat_app/index.js
+++ seng513/chat_app/index.js
@@ -6,46 +6,27 @@ const socket_io = require('socket.io');
 const io = socket_io(server);
 
 
-const PORT = 3000;
+const PORT = 3001;
+console.log(__dirname + '\\index.html');
+
 // this is for static files like my css file, to enable it to work properly
 app.use(express.static(__dirname + '/'));
 // route handler that's called when we hit our website home
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '\\index.html');
 });
 
 //listen on the connection event for incoming sockets
 // if i want to send to everyone except for myself, use socket.broadcast.emit('hi')
-/*
-Basic connection from socket.io tutorial
+//Basic connection from socket.io tutorial
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
 
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        socket.emit('chat message', msg);
     });
-});
 
- */
-io.on('connection', (socket) => {
-    console.log('a user connected');
-/*
-    socket.on('register', handleRegister);
-
-    socket.on('join', handleJoin);
-
-    socket.on('leave', handleLeave);
-
-    socket.on('message', handleMessage);
-    
-    socket.on('chatrooms', handleGetChatrooms);
-
-    socket.on('availableUsers', handleGetAvailableUsers);
-*/
     // disconnect is a pre-defined event
     socket.on('disconnect', () => {
         console.log('client disconnect...', socket.id);
@@ -57,8 +38,9 @@ io.on('connection', (socket) => {
         console.log('received error from client:', socket.id);
         console.log(err);
     });
-});
 
+
+});
 
 // our http server listens to port 3000
 server.listen(PORT, (err) => {
